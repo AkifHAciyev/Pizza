4cfc21739d4e64a8db80816472b55da2d3c03742
AkifHaciyev <akif6563@mail.ru>
2022-06-23 00:19:47 +0400

    add pizza & category render

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import pizzaLogo from '../assets/img/pizza-logo.svg';
 
 const Header = () => {
 	return (
-		<div class="header">
-			<div class="container">
-				<div class="header__logo">
-					<img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+		<div className="header">
+			<div className="container">
+				<div className="header__logo">
+					<img width="38" src={pizzaLogo} alt="Pizza logo" />
 					<div>
 						<h1>React Pizza</h1>
 						<p>самая вкусная пицца во вселенной</p>
 					</div>
 				</div>
-				<div class="header__cart">
-					<a href="/cart.html" class="button button--cart">
+				<div className="header__cart">
+					<a href="/cart.html" className="button button--cart">
 						<span>520 ₽</span>
-						<div class="button__delimiter"></div>
+						<div className="button__delimiter"></div>
 						<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path
 								d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
